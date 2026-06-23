77a2e51e6437894b88f67420be9fd21d45d90f0f
Michał Bugajski <michabugajski2@gmail.com>
2026-06-24 00:05:16 +0200

    Film exercise completed.

--- src/Part4/OOP/MainOOP.java
+++ src/Part4/OOP/MainOOP.java
@@ -1,8 +1,20 @@
 package Part4.OOP;
+import java.util.Scanner;
 
 public class MainOOP {
     public static void main (String[] args) {
-        Song garden = new Song("In The Garden", 10910);
-        System.out.println("The song " + garden.name() + " has a length of " + garden.length() + " seconds.");
+        Film chipmunks = new Film("Alvin and the Chipmunks: The Squeakquel", 0);
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("How old are you?");
+        int age = Integer.valueOf(reader.nextLine());
+
+        System.out.println();
+        if (age >= chipmunks.ageRating()) {
+            System.out.println("You may watch the film " + chipmunks.name());
+        } else {
+            System.out.println("You may not watch the film " + chipmunks.name());
+        }
     }
 }
