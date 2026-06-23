da1b8ceee60b6fe137e238e5f97e220af213b5bc
Michał Bugajski <michabugajski2@gmail.com>
2026-06-23 22:37:22 +0200

    Your first account exercise completed

--- src/MainOOP.java
+++ src/MainOOP.java
@@ -1,5 +1,10 @@
 public class MainOOP {
     public static void main (String[] args) {
-        Account michalAccount = new Account
+        Account michalAccount = new Account("Michal's account", 100.0);
+
+        michalAccount.deposit(20.0);
+
+        System.out.println("The balance of Michal's account is now: " + michalAccount.balance());
+
     }
 }
