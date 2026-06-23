85874c0eb6438a0bd16920cf187c51902daf3a27
Michał Bugajski <michabugajski2@gmail.com>
2026-06-24 01:22:55 +0200

    Statistics(Part 3) exercise completed.

--- src/Part4/OOP/MainOOP.java
+++ src/Part4/OOP/MainOOP.java
@@ -1,14 +1,22 @@
 package Part4.OOP;
+import java.util.Scanner;
 
 public class MainOOP {
     public static void main (String[] args) {
         Statistics statistics = new Statistics();
-        statistics.addNumber(3);
-        statistics.addNumber(5);
-        statistics.addNumber(1);
-        statistics.addNumber(2);
-        System.out.println("Count: " + statistics.getCount());
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (true) {
+            System.out.println("Enter numbers: ");
+            int inputNumbers = Integer.valueOf(scanner.nextLine());
+
+            if (inputNumbers == -1) {
+                break;
+            } else {
+                statistics.addNumber(inputNumbers);
+            }
+        }
         System.out.println("Sum: " + statistics.sum());
-        System.out.println("Average: " + statistics.average());
     }
 }
