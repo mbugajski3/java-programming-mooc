904eb09bee36b762dbbc1522a35628770802de1a
Michał Bugajski <michabugajski2@gmail.com>
2026-06-24 01:08:50 +0200

    Statistics(Part 2) exercise completed.

--- src/Part4/OOP/MainOOP.java
+++ src/Part4/OOP/MainOOP.java
@@ -2,11 +2,13 @@ package Part4.OOP;
 
 public class MainOOP {
     public static void main (String[] args) {
-        Statistics statistics = new Statistics(0);
+        Statistics statistics = new Statistics();
         statistics.addNumber(3);
         statistics.addNumber(5);
         statistics.addNumber(1);
         statistics.addNumber(2);
         System.out.println("Count: " + statistics.getCount());
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Average: " + statistics.average());
     }
 }
