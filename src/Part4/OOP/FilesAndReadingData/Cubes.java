b89394acf583957be31f9e1c6235ee09ad69f35b
Michał Bugajski <michabugajski2@gmail.com>
2026-06-24 20:40:08 +0200

    Cubes exercise completed.

---
+++ src/Part4/OOP/FilesAndReadingData/Cubes.java
@@ -0,0 +1,19 @@
+package Part4.OOP.FilesAndReadingData;
+import java.util.Scanner;
+
+public class Cubes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String inputNumber = scanner.nextLine().trim().toLowerCase();
+
+            if (inputNumber.equals("end")) {
+                break;
+            }
+            int number = Integer.valueOf(inputNumber);
+            int result = number * number * number;
+            System.out.println(result);
+        }
+    }
+}
