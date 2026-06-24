8515a470ddc96f357fb9688de410ea4b297f520a
Michał Bugajski <michabugajski2@gmail.com>
2026-06-24 14:41:35 +0200

    Books exercise completed.

---
+++ src/Part4/OOP/ObjectsInList/Books.java
@@ -0,0 +1,39 @@
+package Part4.OOP.ObjectsInList;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Books {
+    public static void main(String[] args) {
+        ArrayList<Book> booksList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Title: ");
+            String inputTitle = scanner.nextLine();
+
+            if (inputTitle.isEmpty()) {
+                break;
+            }
+            System.out.print("Pages: ");
+            int inputPages = Integer.valueOf(scanner.nextLine());
+            System.out.print("Publication year: ");
+            int inputPublicationYear = Integer.valueOf(scanner.nextLine());
+            booksList.add(new Book(inputTitle, inputPages, inputPublicationYear));
+        }
+        System.out.println();
+        System.out.print("What information will be printed? ");
+        String inputInformation = scanner.nextLine().toLowerCase().trim();
+
+        for (Book book : booksList) {
+            if (inputInformation.equals("everything")) {
+                System.out.println(book);
+            } else if (inputInformation.equals("name")) {
+                System.out.println(book.getTitle());
+            } else if (inputInformation.equals("pages")) {
+                System.out.println(book.getPages());
+            } else if (inputInformation.equals("publication year")){
+                System.out.println(book.getPublicationYear());
+            }
+        }
+    }
+}
