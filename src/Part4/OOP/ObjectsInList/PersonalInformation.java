543dffdc272b0256403838de4adbd9765fc4741f
Michał Bugajski <michabugajski2@gmail.com>
2026-06-24 13:22:46 +0200

    Personal information exercise completed.

---
+++ src/Part4/OOP/ObjectsInList/PersonalInformation.java
@@ -0,0 +1,31 @@
+package Part4.OOP.ObjectsInList;
+
+public class PersonalInformation {
+
+    private String firstName;
+    private String lastName;
+    private String identificationNumber;
+
+    public PersonalInformation(String firstName, String lastName, String identificationNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.identificationNumber = identificationNumber;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getIdentificationNumber() {
+        return this.identificationNumber;
+    }
+
+    @Override
+    public String toString() {
+        return this.firstName + " " + this.lastName;
+    }
+}
---
+++ src/Part4/OOP/ObjectsInList/PersonalInformationCollection.java
@@ -0,0 +1,27 @@
+package Part4.OOP.ObjectsInList;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class PersonalInformationCollection {
+    public static void main (String[] args) {
+        ArrayList<PersonalInformation> personalInformations = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("First name: ");
+            String inputFirstName = scanner.nextLine();
+
+            if (inputFirstName.isEmpty()) {
+                break;
+            }
+            System.out.print("Last name: ");
+            String inputLastName = scanner.nextLine();
+            System.out.print("Identification number: ");
+            String inputIdentificationNumber = scanner.nextLine();
+            personalInformations.add(new PersonalInformation(inputFirstName, inputLastName, inputIdentificationNumber));
+        }
+        for (PersonalInformation information : personalInformations) {
+            System.out.println(information);
+        }
+    }
+}
