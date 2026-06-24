2575f5958f907a107e9ed303a9305106ddb08223
Michał Bugajski <michabugajski2@gmail.com>
2026-06-24 17:22:04 +0200

    Number of Strings exercise completed.

---
+++ src/Part4/OOP/FilesAndReadingData/NumberOfStrings.java
@@ -0,0 +1,20 @@
+package Part4.OOP.FilesAndReadingData;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class NumberOfStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> strings = new ArrayList<>();
+
+        while(true) {
+            String inputString = scanner.nextLine().trim().toLowerCase();
+            if (inputString.equals("end")) {
+                break;
+            } else {
+                strings.add(inputString);
+            }
+        }
+        System.out.println(strings.size());
+    }
+}
