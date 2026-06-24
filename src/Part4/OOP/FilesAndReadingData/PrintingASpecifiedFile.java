b04b86ded689f5266f6f7c0cff5e907fd293e473
Michał Bugajski <michabugajski2@gmail.com>
2026-06-24 21:04:30 +0200

    Printing a Specified File exercise completed.

---
+++ src/Part4/OOP/FilesAndReadingData/PrintingASpecifiedFile.java
@@ -0,0 +1,20 @@
+package Part4.OOP.FilesAndReadingData;
+import java.util.Scanner;
+import java.nio.file.Paths;
+
+public class PrintingASpecifiedFile {
+    public static void main (String[] args) {
+        Scanner inputKeyboardReader = new Scanner(System.in);
+        System.out.println("Which file should have its contents printed?");
+        String inputFile = inputKeyboardReader.nextLine();
+
+        try (Scanner fileReader = new Scanner(Paths.get(inputFile))) {
+            while (fileReader.hasNextLine()) {
+                String fileRow = fileReader.nextLine();
+                System.out.println(fileRow);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
