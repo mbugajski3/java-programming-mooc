29254ca0f6161f449f3018d0af0deac9729d6565
Michał Bugajski <michabugajski2@gmail.com>
2026-06-25 16:04:34 +0200

    Health station(Part 3) exercise completed.

--- src/Part5/OOPProgramming/ObjectsAndReferences/HealthStation.java
+++ src/Part5/OOPProgramming/ObjectsAndReferences/HealthStation.java
@@ -2,12 +2,18 @@ package Part5.OOPProgramming.ObjectsAndReferences;
 
 public class HealthStation {
 
+    int weighCount = 0;
+
     public int weigh(Person person) {
+        weighCount++;
         return person.getWeight();
     }
     public void feed(Person person) {
         int actualWeight = this.weigh(person);
         int newWeight = actualWeight + 1;
         person.setWeight(newWeight);
+    }
+    public int weighings() {
+        return weighCount;
     }
 }
