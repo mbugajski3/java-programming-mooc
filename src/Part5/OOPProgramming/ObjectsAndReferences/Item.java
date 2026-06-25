c946c24f9ebfcd1cdd5ed59b29f2fce3666d5ae0
Michał Bugajski <michabugajski2@gmail.com>
2026-06-25 22:13:24 +0200

    Archive(Part 1) exercise completed.

---
+++ src/Part5/OOPProgramming/ObjectsAndReferences/Item.java
@@ -0,0 +1,31 @@
+package Part5.OOPProgramming.ObjectsAndReferences;
+
+public class Item {
+    private String itemIdentifier;
+    private String itemName;
+
+    public Item(String itemName, String itemIdentifier) {
+        this.itemIdentifier = itemIdentifier;
+        this.itemName = itemName;
+    }
+
+    @Override
+    public boolean equals(Object compared) {
+        if (this == compared) {
+            return true;
+        }
+        if (!(compared instanceof Item)) {
+            return false;
+        }
+
+        Item comparedItem = (Item) compared;
+
+        return (this.itemIdentifier.equals(comparedItem.itemIdentifier) &&
+                this.itemName.equals(comparedItem.itemName));
+    }
+
+    @Override
+    public String toString() {
+        return this.itemIdentifier + ": " + this.itemName;
+    }
+}
---
+++ src/Part5/OOPProgramming/ObjectsAndReferences/ItemList.java
@@ -0,0 +1,32 @@
+package Part5.OOPProgramming.ObjectsAndReferences;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class ItemList {
+    public static void main(String[] args) {
+        ArrayList<Item> itemList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Identifier?");
+            String inputIdentifier = scanner.nextLine();
+
+            if (inputIdentifier.isEmpty()) {
+                break;
+            }
+            System.out.println("Name?");
+            String inputName = scanner.nextLine();
+
+            if (inputName.isEmpty()) {
+                break;
+            }
+            Item item = new Item(inputIdentifier, inputName);
+            itemList.add(item);
+
+        }
+        System.out.println("==Items==");
+        for (Item item : itemList) {
+            System.out.println(item);
+        }
+    }
+}
