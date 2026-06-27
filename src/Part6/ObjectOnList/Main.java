559f566688de2d83c303b6d6b93e15dfbe404021
Michał Bugajski <michabugajski2@gmail.com>
2026-06-27 18:19:53 +0200

    MessagingService exercise completed.

--- src/Part6/ObjectOnList/Main.java
+++ src/Part6/ObjectOnList/Main.java
@@ -1,16 +1,30 @@
 package Part6.ObjectOnList;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Stack s = new Stack();
-        s.add("1");
-        s.add("2");
-        s.add("3");
-        s.add("4");
-        s.add("5");
+        MessagingService service = new MessagingService();
 
-        while (!s.isEmpty()) {
-            System.out.println(s.take());
+        Message first = new Message("Michael", "Hello, this is a short message.");
+        Message second = new Message("Adam", "This message should also be added.");
+
+        String exactly280 = "a".repeat(280);
+        Message third = new Message("Anna", exactly280);
+
+        String tooLong = "b".repeat(281);
+        Message fourth = new Message("John", tooLong);
+
+        service.add(first);
+        service.add(second);
+        service.add(third);
+        service.add(fourth);
+
+        ArrayList<Message> messages = service.getMessages();
+
+        System.out.println("Number of accepted messages: " + messages.size());
+
+        for (Message message : messages) {
+            System.out.println(message);
         }
     }
 }
---
+++ src/Part6/ObjectOnList/Message.java
@@ -0,0 +1,24 @@
+package Part6.ObjectOnList;
+
+public class Message {
+
+    private String sender;
+    private String content;
+
+    public Message(String sender, String content) {
+        this.sender = sender;
+        this.content = content;
+    }
+
+    public String getSender() {
+        return this.sender;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public String toString() {
+        return this.sender + ": " + this.content;
+    }
+}
